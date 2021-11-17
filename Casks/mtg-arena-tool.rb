ea0ddf5050f2a7c65e6d8a3135c1beb58feb6bc6
Auto Updater <actions@users.noreply.github.com>
2021-11-17 20:08:19 +0000

    Auto-update TWS Casks: Wed Nov 17 20:08:19 UTC 2021

--- Casks/mtg-arena-tool.rb
+++ Casks/mtg-arena-tool.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "mtg-arena-tool" do
-  version "6.1.9"
+  version "6.1.10"
   sha256 :no_check
 
   url "https://github.com/mtgatool/mtgatool-desktop/releases/download/v#{version}/mtgatool-desktop-#{version}.pkg",
